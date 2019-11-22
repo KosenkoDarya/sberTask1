1fb1a76175c9bba382c841dbe75f3f54b3922188
DaryaKosenko <KosenkoDarya64@gmail.com>
2019-11-22 14:28:08 +0400

    Window

---
+++ src/main/Controller.java
@@ -0,0 +1,19 @@
+package main;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+public class Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
+}
